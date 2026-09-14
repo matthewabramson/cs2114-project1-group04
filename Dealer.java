8486447e4d74a51d24a825186b90d493e540efc4
drudra2026S <drudra@vt.edu>
2026-09-14 14:59:44 -0400

    Extend Dealer class to inherit from Hand
    Extend Dealer class from Hand to inherit functionality.

--- Dealer.java
+++ Dealer.java
@@ -1,3 +1,13 @@
-public class Dealer {
+public class Dealer extends Hand {
+ 
+  private
+
+
+
+
+  // Methods Dealer classes uses:
+  //public Hand getHand(){} - Returns the dealer’s current hand
+  //public boolean shouldHit(){} - determines whether the dealer should receive another card
+  // public void clearHand(){} - clears the dealers current hand
 
 }
