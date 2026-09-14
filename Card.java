83c2f42453682d5ce3e4562837724a3342254745
mabramson492026S <mabramson49@vt.edu>
2026-09-14 14:46:04 -0400

    update the class and add getter methods

---
+++ Card.java
@@ -0,0 +1,23 @@
+public class Card {
+
+  private String rank;
+  private int value;
+
+  public card (String rank, int value)
+  {
+    this.rank = rank;
+    this.value = value;
+  }
+
+  public String getRank()
+  {
+    return rank;
+  }
+
+  public int getValue()
+  {
+    return value;
+  }
+
+
+}
