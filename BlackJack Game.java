7e4cefe7ac82e6cbeda664d597980074c198dab8
drudra2026S <drudra@vt.edu>
2026-09-14 15:30:04 -0400

    Initialize BlackJackGame class and fix syntax
    Fix syntax errors and initialize game components.

--- BlackJack Game.java
+++ BlackJack Game.java
@@ -1,3 +1,40 @@
 public class BlackJackGame { 
+  private Deck deck;
+  private Player player; 
+  private Dealer dealer; 
+  private Statistics record; 
+
+  public BlackJackGame() {
+    deck = new Deck();
+    player = new Player(); 
+    dealer = new Dealer();
+    record = new Statistics();
+  }
+
+  // returns player's hand 
+  public Hand getHand() { 
+    return player.getHand();
+  }
+
+  // starts the game 
+  public void play() {
+    System.out.printIn("Starting Blackjack... ");
+    deck.shuffle();
+
+    boolean continuePlaying = true;
+
+    while continuePlaying { 
+      playRound();
+
+      // Come back later to add input logic to ask if user wants to continue 
+      continuePlaying = false;
+    }
+
+    System.out.printIn("Game Over!");
+    record.printStats();
+  }
+      
+    
+  
 
 }
