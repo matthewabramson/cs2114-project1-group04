1d853c9e0349b0c2dab4ac7208aebaf7e8e0ea78
Jacobd062026S <jacobd06@vt.edu>
2026-09-14 16:36:40 -0400

    Hand class
    Fix typos and syntax errors in Hand class.

--- Hand.java
+++ Hand.java
@@ -1,10 +1,23 @@
 public class Hand{
 
-  privat ArrayList<Cards> cards;
+  private ArrayList<Cards> cards;
 
   import.java.until.ArrayList;
 
-  public
+  public Hand(){
+    cards = new ArrayList<Cards>
+  }
+  pubic void addCard(Card card){
+    cards.add(card);
+  }
+
+  public int getTotal(){
+    int total = 0
+    int aces = 0
+
+    for (Card card
+    
+    
 
   
 
