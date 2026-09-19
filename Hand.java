5af87bd3f61f29c54fa3d7eb0eba56db64a3353b
Jacobd062026S <jacobd06@vt.edu>
2026-09-19 19:51:12 -0400

    Hand.java
    Fix syntax errors and improve getTotal method logic.

--- Hand.java
+++ Hand.java
@@ -15,7 +15,23 @@ public class Hand{
     int total = 0
     int aces = 0
 
-    for (Card card
+    for (Card card: cards) {
+            total += card.getValue();
+
+            if (card.isAce()) {
+                aces++;
+            }
+        }
+
+        // Change an Ace from 11 to 1 if the hand would bust
+        while (total > 21 && aces > 0) {
+            total -= 10;
+            aces--;
+        }
+
+        return total;
+    }
+}
     
     
 
