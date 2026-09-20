72d1e493c9f656cc18a91dea6a7d72135fbf4441
drudra2026S <drudra@vt.edu>
2026-09-20 14:05:18 -0400

    Refactor BlackJack game logic and winner determination
    Fixed issues with play again logic and betting. Refactored determineWinner method for clarity and correctness.

--- BlackJack Game.java
+++ BlackJack Game.java
@@ -23,13 +23,15 @@ public class BlackJackGame {
       playRound();
 
       String again = input.getPlayAgain();
-      playAgain = again.equalsIgnoreCase("again");
-      //#
+      
+      if (again.equalsIsIgnoreCase("leave")) {
+        playAgain = false
+      }
     }
 
     System.out.println("Final Stats:");
     System.out.println("Wins: " + stats.getWins());
-    System.out.println("Losses: " + stats.getLosses());
+    System.out.println("Losses; " + stats.getLosses());
   }
 
   //runs each round of BlackJack
@@ -40,50 +42,21 @@ public class BlackJackGame {
     }
 
     dealer.clearHand();
-    player.clearHand();
+    player.getHand().clear();
 
     int bet = input.getBet(); 
     if (!player.placeBet(bet)) { 
       System.out.println("Invalid Bet."); 
       return;
     }
-  }
-    public void determineWinner(){
-      int playerTotal = player.getHand().getTotal();
-      int dealerTotal = dealer.getHand().getTotal();
 
-      if (playerTotal > 21){
-        System.out.println("Dealer Wins!");
-        player.subtractBet();
-        stats.addLoss();
-      }
-      else if (dealerTotal > 21) {
-        System.out.println("Player Wins!");
-        player.addWinnings();
-        stats.addWin();
-      }
-      else if (playerTotal > dealerTotal) {
-        System.out.println("Player Wins!");
-        player.addWinnings();
-        stats.addWin();
-      }
-    
-      else if (dealerTotal > playerTotal) {
-        System.out.println("Dealer Wins!");
-        player.subtractBet();
-        stats.addLoss();
-      }
-      else {
-        System.out.println("It is a tie");
-      }
-    }
-        
-    
+    player.subtractBet();
 
     // The Initial deal 
     player.getHand().addCard(deck.dealCard());
     player.getHand().addCard(deck.dealCard()); 
     dealer.getHand().addCard(deck.dealCard()); 
+    
     // show player total
     System.out.println("Player Hand Total: " + player.getHand().getTotal());
 
@@ -96,14 +69,15 @@ public class BlackJackGame {
     while (playerTurn) { 
       String choice = input.getHitOrStay();
 
-      if (choice.equalsIgnoreCase("hit")) {
+      if (choice.equalsIsIgnoreCase("hit")) {
         player.getHand().addCard(deck.dealCard());
         System.out.println("Player Total: " + player.getHand().getTotal());
 
         // When the player busts so dealer dosen't draw
         if (player.getHand().getTotal() > 21) { 
           System.out.println("Player busts!");
-          determineWinner();
+          System.out.println("You lose!");
+          stats.recordLoss(); 
           return;
         }
       }
@@ -119,4 +93,39 @@ public class BlackJackGame {
     }
 
     determineWinner();
+}
+  
+  public void determineWinner() {
+
+    int playerTotal = player.getHand().getTotal();
+    int dealerTotal = dealer.getHand().getTotal();
+
+    if (dealerTotal > 21) {
+
+        stats.recordWin();
+        player.addWinnings();
+
+        System.out.println("Dealer busts. You win!");
+    }
+    else if (playerTotal > dealerTotal) {
+
+        stats.recordWin();
+        player.addWinnings();
+
+        System.out.println("You win!");
+    }
+    else if (playerTotal == dealerTotal) {
+
+        stats.recordPush();
+        player.returnBet();
+
+        System.out.println("Push.");
+    }
+    else {
+
+        stats.recordLoss();
+
+        System.out.println("You lose.");
+      }
+  }
 }
