7a81c2a1f03d9463f6f49d8a8b0936302e495e41
Jacobd062026S <jacobd06@vt.edu>
2026-09-20 17:29:38 -0400

    Update Card Test

--- src/blackjack/CardTest.java
+++ src/blackjack/CardTest.java
@@ -1,5 +1,45 @@
 package blackjack;
 
-public class CardTest extends student.TestCase{
+public class CardTest
+    extends student.TestCase
+{
+    private Card card1;
+    private Card card2;
+
+    public void setUp()
+    {
+        card1 = new Card("Ace", "♠\uFE0F", 11);
+        card2 = new Card("10", "♥\uFE0F", 10);
+    }
+
+
+    /**
+     * Test the Card get rank method
+     */
+    public void testGetRank()
+    {
+        assertEquals(card1.getRank(), "Ace");
+        assertEquals(card2.getRank(), "10");
+    }
+
+
+    /**
+     * Test the Card get value method.
+     */
+    public void testGetValue()
+    {
+        assertEquals(card1.getValue(), 11);
+        assertEquals(card2.getValue(), 10);
+    }
+
+
+    /** 
+     * Test the Card get suit method.
+     */
+    public void testGetSuit()
+    {
+        assertEquals(card1.getSuit(), "♠\uFE0F");
+        assertEquals(card2.getSuit(), "♥\uFE0F");
+    }
 
 }
