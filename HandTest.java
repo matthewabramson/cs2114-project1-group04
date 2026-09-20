754b64cf8fbb2ee3b77aeb5d5d5f556543fc2bf5
Jacobd062026S <jacobd06@vt.edu>
2026-09-19 20:01:56 -0400

    HandTest.java

---
+++ HandTest.java
@@ -0,0 +1,73 @@
+import student.TestCase;
+
+public class HandTest extends TestCase {
+
+    private Hand hand;
+
+    public void setUp() {
+        hand = new Hand();
+    }
+
+    public void testNewHand() {
+        assertEquals(0, hand.getTotal());
+    }
+
+    public void testAddCard() {
+        Card card = new Card("5", 5);
+
+        hand.addCard(card);
+
+        assertEquals(5, hand.getTotal());
+    }
+
+    public void testMultipleCards() {
+        hand.addCard(new Card("5", 5));
+        hand.addCard(new Card("7", 7));
+
+        assertEquals(12, hand.getTotal());
+    }
+
+    public void testFaceCards() {
+        hand.addCard(new Card("King", 10));
+        hand.addCard(new Card("Queen", 10));
+
+        assertEquals(20, hand.getTotal());
+    }
+
+    public void testAceAsEleven() {
+        hand.addCard(new Card("Ace", 11));
+        hand.addCard(new Card("9", 9));
+
+        assertEquals(20, hand.getTotal());
+    }
+
+    public void testAceAsOne() {
+        hand.addCard(new Card("Ace", 11));
+        hand.addCard(new Card("9", 9));
+        hand.addCard(new Card("5", 5));
+
+        // 11 + 9 + 5 = 25
+        // Ace changes from 11 to 1
+        // 1 + 9 + 5 = 15
+        assertEquals(15, hand.getTotal());
+    }
+
+    public void testMultipleAces() {
+        hand.addCard(new Card("Ace", 11));
+        hand.addCard(new Card("Ace", 11));
+        hand.addCard(new Card("9", 9));
+
+        // 11 + 11 + 9 = 31
+        // One Ace becomes 1
+        // 1 + 11 + 9 = 21
+        assertEquals(21, hand.getTotal());
+    }
+
+    public void testBust() {
+        hand.addCard(new Card("King", 10));
+        hand.addCard(new Card("Queen", 10));
+        hand.addCard(new Card("5", 5));
+
+        assertEquals(25, hand.getTotal());
+    }
+}
