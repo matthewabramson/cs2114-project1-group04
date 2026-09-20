173dd8b796f1f04d8911e2a3f2bf933bbbf18cd2
Jacobd062026S <jacobd06@vt.edu>
2026-09-20 12:34:48 -0400

    Enhance input validation for betting and choices
    Refactored input handling methods to improve validation for user inputs.

--- InputHandler.java
+++ InputHandler.java
@@ -9,12 +9,36 @@ public class InputHandler{
     public static int getBet(){
 
         System.out.print("Please Enter Your Bet: $");
+        while (!scanner.hasNextInt()){
+            System.out.println("Please enter a whole number.");
+            scanner.next();
+            System.out.print("Please Enter Your Bet: $");
 
-        int bet = scanner.nextInt();
-        return bet;
+        }
+        return scanner.nextInt();
 
     }
+    public static String getHitOrStay(){
+        System.out.print("Hit or Stand?");
+        String choice = scanner.next();
 
-
-
+        while (!choice.equalsIgnoreCase("hit")
+               && !choice.equalsIgnoreCase("stand")){
+            System.out.println("Please enter hit or stand.");
+            System.out.print("Hit or Stand? ");
+            choice = scanner.next();
+        }
+        return choice;
+    }
+    public static String getPlayAgain(){
+        System.out.print("Play again or leave? ");
+        String choice = scanner.next();
+        while (!choice.equalsIgnoreCase("again")
+               && !choice.equalsIgnoreCase("leave")){
+            System.out.println("Please enter again or leave.");
+            System.out.print("Play again or leave?");
+            choice = scanner.next();
+        }
+        return choice;
+    }
 }
